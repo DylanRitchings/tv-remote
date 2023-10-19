eb36fa429f61a2d56f5985bf828cd605f94ee727
DylanRitchings <43646210+DylanRitchings@users.noreply.github.com>
2023-10-19 20:19:58 +0100

    Update scanner.c

--- scanner.c
+++ scanner.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-// Define the GPIO pin where the IR receiver is connected
-#define IR_GPIO_PIN 17 // Replace with the actual GPIO pin number
+#define IR_GPIO_PIN 27 
 
 int main() {
     if (wiringPiSetup() == -1) {
@@ -10,21 +9,17 @@ int main() {
         return 1;
     }
 
-    // Set the GPIO pin to input mode
     pinMode(IR_GPIO_PIN, INPUT);
 
     printf("IR receiver initialized. Listening for IR messages...\n");
 
     while (1) {
         //if (digitalRead(IR_GPIO_PIN) == HIGH) {
-            // IR signal detected
         int value = digitalRead(IR_GPIO_PIN);
-        printf("Raw IR Value: %d\n", value);
-        delay(100); // Adjust the delay as needed
+        printf("IR Value: %d\n", value);
+        delay(1); 
         //}
     }
-
-    // The cleanup code will not be reached in this example, as the loop is infinite
 
     return 0;
 }
